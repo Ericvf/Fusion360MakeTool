5d192b080dde1b2fc69fbfdaba56c83f96def15c
Eric van Feggelen <e.feggelen@gmail.com>
2019-08-14 23:17:36 +0200

    Add project files.

---
+++ ConsoleApp1/Kernel32.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Kernel32
+    {
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/aa364963.aspx
+        /// </summary>
+        /// <param name="lpFileName"></param>
+        /// <param name="nBufferLength"></param>
+        /// <param name="lpBuffer"></param>
+        /// <param name="lpFilePart"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+        public static extern uint GetFullPathNameW(
+           string lpFileName,
+           uint nBufferLength,
+           StringBuilder lpBuffer,
+           IntPtr lpFilePart);
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms686206.aspx
+        /// </summary>
+        /// <param name="lpName"></param>
+        /// <param name="lpValue"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)]
+        internal static extern bool SetEnvironmentVariableW(
+            string lpName,
+            string lpValue);
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!args.Any())
+                    MessageBox.Show("No file path specified. Aborted.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                var stringBuilder = new StringBuilder(2048);
+                Kernel32.GetFullPathNameW(args[0], (uint)stringBuilder.Capacity, stringBuilder, IntPtr.Zero);
+                var convertedPath = stringBuilder.ToString();
+
+                Clipboard.SetData(DataFormats.Text, convertedPath);
+
+                MessageBox.Show("The path has been copied to the clipboard. Use the paste function in your slicer to add the file."
+                    + Environment.NewLine + Environment.NewLine + convertedPath, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
